73864e6c7959957f2e3add05e915160df2a70373
Guilherme Campos <guilhermecampos100@gmail.com>
2015-07-17 21:57:59 -0300

    home ja ficando com cara de app

--- js/app.js
+++ js/app.js
@@ -29,24 +29,9 @@
 
       data.items = [
           {
-              title: 'Item 1 Title',
-              label: '4h',
-              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          },
-          {
-              title: 'Another Item Title',
-              label: '6h',
-              desc: 'Ut enim ad minim veniam.'
-          },
-          {
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-          },
-          {
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+              title: 'Chamar Garçom',
+              label: '0:22s',
+              desc: 'Damiano Macedo'
           }
       ];
 
